75210b5994a8150b6d75a4e22695bc387c197633
LawlietAnime <ryusakilawlietgithub@gmail.com>
2023-01-01 23:25:28 +0100

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Inumaki on 01.01.23.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
